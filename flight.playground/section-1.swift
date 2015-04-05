9d0d218362c42bf10c682b37d79c4b6be4c2af6c
Lucas <lucas@agtlucas.com>
2015-04-04 21:12:46 -0300

    Updates

--- flight.playground/section-1.swift
+++ flight.playground/section-1.swift
@@ -9,3 +9,26 @@ let view = UIView(frame: bounds)
 view.backgroundColor = UIColor.lightGrayColor()
 
 XCPShowView("view", view)
+
+func dot() -> CALayer {
+    let layer = CALayer()
+    layer.bounds = CGRect(x: 0, y: 0, width: 4, height: 4)
+    layer.cornerRadius = 2
+    layer.backgroundColor = UIColor.redColor().CGColor
+    return layer
+}
+
+let bank = 30.0 * M_PI / 180.0
+let velocity = 15.0
+let initial = (x: 100.0, y: 100.0)
+let vector = (x: 300.0, y: 120.0)
+let heading = 25.0 * M_PI / 180.0
+let left = false
+
+let plane = dot()
+plane.position = CGPoint(x: initial.x, y: initial.y)
+view.layer.addSublayer(plane)
+
+let vectorLayer = dot()
+vectorLayer.position = CGPoint(x: vector.x, y: vector.y)
+view.layer.addSublayer(vectorLayer)
