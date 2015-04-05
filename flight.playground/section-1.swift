c2f00f5f1233dad88f05e9cd9a2781f403a69b75
Lucas <lucas@agtlucas.com>
2015-04-04 21:04:18 -0300

    Updates

--- flight.playground/section-1.swift
+++ flight.playground/section-1.swift
@@ -4,4 +4,8 @@ import UIKit
 import XCPlayground
 import VectorKit
 
+let bounds = CGRect(x: 0, y: 0, width: 400, height: 200)
+let view = UIView(frame: bounds)
+view.backgroundColor = UIColor.lightGrayColor()
 
+XCPShowView("view", view)
